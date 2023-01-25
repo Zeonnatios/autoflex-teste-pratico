1351710303dd0ebe03638c9f6b4fba8b193b12e9
Matheus Antonio <matheusantonio232@gmail.com>
2023-01-25 15:29:25 -0300

    feat: creating product model

---
+++ backend/factory-management/src/main/java/com/projetada/factorymanagement/models/Product.java
@@ -0,0 +1,44 @@
+package com.projetada.factorymanagement.models;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PRODUCTS")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false)
+    private double value;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
