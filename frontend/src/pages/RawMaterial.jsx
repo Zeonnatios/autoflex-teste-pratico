39ce284f418406e5edb50449ef544d3048f7617f
Matheus Antonio <matheusantonio232@gmail.com>
2023-01-25 23:05:22 -0300

    feat: add RawMaterial page

---
+++ frontend/src/pages/RawMaterial.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+function RawMaterial() {
+  return (
+
+    <div>
+      <Header />
+    </div>
+
+  );
+}
+
+export default RawMaterial;
