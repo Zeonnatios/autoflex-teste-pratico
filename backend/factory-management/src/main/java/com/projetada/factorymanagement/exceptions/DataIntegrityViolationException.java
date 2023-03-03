28894913264e77243001ac061ebdf181ac19da20
Matheus Antonio <matheusantonio232@gmail.com>
2023-03-03 00:23:07 -0300

    feat: add exceptions

---
+++ backend/factory-management/src/main/java/com/projetada/factorymanagement/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,8 @@
+package com.projetada.factorymanagement.exceptions;
+
+public class DataIntegrityViolationException extends RuntimeException {
+
+    public DataIntegrityViolationException(String message) {
+        super(message);
+    }
+}
